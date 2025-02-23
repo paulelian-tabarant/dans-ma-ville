4fd1d083f3aad891573522e216422efaec49802d
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-02-23 16:05:09 +0100

    fix(back): lint

--- backend/src/tests/main.py
+++ backend/src/tests/main.py
@@ -1,11 +1,12 @@
-from fastapi.testclient import TestClient
+from http import HTTPStatus
 
 from backend.src.main import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
 
 def test_health_check() -> None:
     response = client.get("/health")
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {"status": "healthy"}
