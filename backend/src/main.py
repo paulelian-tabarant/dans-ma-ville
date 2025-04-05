86d4192c245ee21e03e9e7810249563169581fc2
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-04-05 18:38:46 +0200

    hello world qui fonctionne

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="Dans ma ville")
 
@@ -6,3 +7,12 @@ app = FastAPI(title="Dans ma ville")
 @app.get("/health")
 async def health_check() -> dict[str, str]:
     return {"status": "healthy"}
+
+
+class Personne(BaseModel):
+    prenom: str
+
+
+@app.post('/bonjour')
+async def bonjour(personne: Personne) -> dict[str, str]:
+    return {"message": "Bonjour, " + personne.prenom + " !"}
--- backend/src/tests/test_bonjour.py
+++ backend/src/tests/test_bonjour.py
@@ -6,6 +6,6 @@ client = TestClient(app)
 
 
 def test_doit_dire_bonjour_avec_le_nom_de_la_personne():
-    response = client.post('/bonjour', json={"nom": "Jean-Pierre"})
+    response = client.post('/bonjour', json={"prenom": "Jean-Pierre"})
     assert response.status_code == 200
-    assert response.json() == {"message": "Bonjour Jean-Pierre !"}
+    assert response.json() == {"message": "Bonjour, Jean-Pierre !"}
