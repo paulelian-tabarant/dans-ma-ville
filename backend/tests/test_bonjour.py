849620086faf3c94ff40d4e64847e3c4214e083f
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-06-27 17:13:24 +0200

    ci: ajouter check de syntax au step compile

--- backend/tests/test_bonjour.py
+++ backend/tests/test_bonjour.py
@@ -5,7 +5,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_doit_dire_bonjour_avec_le_nom_de_la_personne():
+def test_doit_dire_bonjour_avec_le_nom_de_la_personne() -> None:
     response = client.post('/bonjour', json={"prenom": "Jean-Pierre"})
     assert response.status_code == 200
     assert response.json() == {"message": "Bonjour, Jean-Pierre !"}
