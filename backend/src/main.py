08c57e0a75f2507188ef05226ac9721bb102b609
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-06-28 12:58:29 +0200

    fix lint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
+from starlette.requests import Request
+
 # Nécessaire pour que la racine du projet puisse être résolue en termes d'imports
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from fastapi import FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
-from starlette.requests import Request
 
 from routers import bonjour
 
@@ -20,5 +21,7 @@ async def health_check() -> dict[str, str]:
 
 
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, error: RequestValidationError) -> None:
+async def validation_exception_handler(
+    _: Request, error: RequestValidationError
+) -> None:
     raise HTTPException(status_code=400, detail=error.errors())
--- backend/tests/test_bonjour.py
+++ backend/tests/test_bonjour.py
@@ -6,11 +6,11 @@ client = TestClient(app)
 
 
 def test_doit_dire_bonjour_avec_le_nom_de_la_personne() -> None:
-    response = client.post('/bonjour', json={"prenom": "Jean-Pierre"})
+    response = client.post("/bonjour", json={"prenom": "Jean-Pierre"})
     assert response.status_code == 200
     assert response.json() == {"message": "Bonjour, Jean-Pierre !"}
 
 
 def test_doit_retourner_400_si_pas_de_nom_fourni() -> None:
-    response = client.post('/bonjour', json={})
+    response = client.post("/bonjour", json={})
     assert response.status_code == 400
