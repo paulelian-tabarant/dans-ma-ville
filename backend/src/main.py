84b7306c3dfb35bdf77b3396b56b7d9735aff100
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-06-28 12:28:01 +0200

    good

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Nécessaire pour que la racine du projet puisse être résolue en termes d'imports
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
 from starlette.requests import Request
