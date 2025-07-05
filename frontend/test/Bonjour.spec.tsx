09b8a112b07acb5edc081a0af42ea85bc93fc9cd
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-07-05 21:09:51 +0200

    refactor

--- frontend/test/Bonjour.spec.tsx
+++ frontend/test/Bonjour.spec.tsx
@@ -1,4 +1,4 @@
-import { beforeAll, describe, expect, it } from "vitest";
+import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest";
 import { render, waitFor } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import { http, HttpResponse } from "msw";
@@ -7,14 +7,22 @@ import Bonjour from "../src/components/Bonjour.tsx";
 import type { BonjourRequestBody } from "../src/hooks/useBonjour.ts";
 
 describe('Bonjour', () => {
+    const endpointUrl = 'http://localhost:8000/bonjour/'
+
     beforeAll(() => {
         server.listen()
     })
 
+    afterEach(() => {
+        server.resetHandlers()
+    })
+
+    afterAll(() => {
+        server.close()
+    })
+
     it("doit envoyer le nom saisi", async () => {
         const prenom = "Jean"
-
-        const endpointUrl = 'http://localhost:8000/bonjour/'
 
         let requestBody: BonjourRequestBody
         server.use(http.post(endpointUrl, async ({ request }) => {
