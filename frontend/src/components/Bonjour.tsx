905fdc93f3762079260154a32df1c08cb2aad025
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-07-05 18:01:15 +0200

    refactor

--- frontend/src/components/Bonjour.tsx
+++ frontend/src/components/Bonjour.tsx
@@ -4,12 +4,9 @@ import { useBonjour } from "../hooks/useBonjour.ts";
 
 function Bonjour() {
     const [prenom, setPrenom] = useState('')
-
     const { isLoading, response, postBonjour } = useBonjour()
 
-    const onBonjourClick = async () => {
-        await postBonjour(prenom)
-    }
+    const onBonjourClick = async () => postBonjour(prenom)
 
     return (
         <>
@@ -17,8 +14,8 @@ function Bonjour() {
 
             <label htmlFor="prenom">Entre ici ton prénom :</label>
             <input name="prenom" type="text" onChange={e => setPrenom(e.target.value)}/>
-
             <button onClick={onBonjourClick}>Envoyer</button>
+
             {isLoading && <p>Loading...</p>}
             {!isLoading && response && <p>{response.message}</p>}
         </>
