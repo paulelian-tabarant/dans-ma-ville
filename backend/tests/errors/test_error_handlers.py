ed89cf87526fe9e82595af3798df4a1a0dbdb5a9
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-07-05 20:39:06 +0200

    fix: erreur 400 quand pas de json valide fourni

--- backend/tests/errors/test_error_handlers.py
+++ backend/tests/errors/test_error_handlers.py
@@ -26,3 +26,12 @@ def test_doit_fournir_toutes_les_erreurs_de_validation_du_request_body() -> None
     assert response.json() == {
         "message": "Field required: prenom; Field required: age; Field required: est_actif"
     }
+
+
+def test_doit_fournir_seulement_le_message_quand_pas_plus_de_details() -> None:
+    response = client.post("/test", json="")
+
+    assert response.status_code == 400
+    assert response.json() == {
+        "message": "Input should be a valid dictionary or object to extract fields from"
+    }
