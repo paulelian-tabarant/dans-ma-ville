6ecea5479dfcb92751f09f0ea187ce7cd36b433e
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2025-07-06 12:23:18 +0200

    minor improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,8 @@ import Bonjour from "./components/Bonjour.tsx";
 
 function App() {
     return (
-        <main className="flex flex-col items-center py-14">
-            <div className="w-5/6">
-                <Bonjour/>
-            </div>
+        <main className="w-5/6">
+            <Bonjour/>
         </main>
     )
 }
