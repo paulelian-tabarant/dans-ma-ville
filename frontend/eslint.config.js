6c989333de8bdf67f794e56da03e08c86e303f67
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2025-07-06 15:16:11 +0200

    lint: règles react

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,7 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import {globalIgnores} from 'eslint/config'
+import reactDom from 'eslint-plugin-react-dom'
+import react from "eslint-plugin-react-x";
 
 export default tseslint.config([
     globalIgnores(['dist']),
@@ -20,6 +22,10 @@ export default tseslint.config([
             ...tseslint.configs.strictTypeChecked,
             reactHooks.configs['recommended-latest'],
             reactRefresh.configs.vite,
+            // Enable lint rules for React
+            react.configs.recommended,
+            // Enable lint rules for React DOM
+            reactDom.configs.recommended,
         ],
         languageOptions: {
             ecmaVersion: 2020,
