4b3768513f900d8397951008ffb024e178d262b5
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2025-07-06 10:00:31 +0200

    base style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,11 @@ import Bonjour from "./components/Bonjour.tsx";
 
 function App() {
     return (
-        <main>
-            <h1>Hello, Vite</h1>
-            <Bonjour/>
+        <main className="flex flex-col items-center py-14">
+            <div className="w-5/6">
+                <h1>Hello, Vite</h1>
+                <Bonjour/>
+            </div>
         </main>
     )
 }
--- frontend/src/components/Bonjour.tsx
+++ frontend/src/components/Bonjour.tsx
@@ -3,6 +3,7 @@ import { useApi } from "../hooks/useApi.ts";
 import type { BonjourRequestBody, BonjourResponseBody } from "../hooks/useBonjour.ts";
 import { Button } from "@/components/ui/button.tsx";
 import { Input } from "@/components/ui/input.tsx";
+import { Alert, AlertDescription } from "@/components/ui/alert.tsx";
 
 
 function Bonjour() {
@@ -18,13 +19,15 @@ function Bonjour() {
             <h2>Bonjour !</h2>
 
             <label htmlFor="prenom">Entre ici ton prénom :</label>
-            <div className="flex gap-2">
-                <Input id="prenom" type="text" onChange={e => setPrenom(e.target.value)}/>
-                <Button onClick={onBonjourClick}>Envoyer</Button>
-            </div>
+            <div className="flex flex-col gap-2">
+                <div className="flex gap-2">
+                    <Input id="prenom" type="text" onChange={e => setPrenom(e.target.value)}/>
+                    <Button onClick={onBonjourClick}>Envoyer</Button>
+                </div>
 
-            {isLoading && <p>Loading...</p>}
-            {!isLoading && response && <p>{response.message}</p>}
+                {isLoading && <p>Loading...</p>}
+                {!isLoading && response && <Alert><AlertDescription>{response.message}</AlertDescription></Alert>}
+            </div>
         </>
     )
 }
