2ae39535ca317efac306bb6f178a1e1a54851d3c
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-07-14 18:08:35 +0200

    nitpicks

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import {globalIgnores} from 'eslint/config'
+import { globalIgnores } from 'eslint/config'
 import reactDom from 'eslint-plugin-react-dom'
 import react from "eslint-plugin-react-x";
 import stylistic from '@stylistic/eslint-plugin'
